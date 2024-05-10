01056ab73a239bb987ff78c050fa3b2cfb381fa7
Geetha Sidhambaram <sidh.geetha@gmail.com>
2024-05-10 22:45:14 +0100

    rect router and folder creation done

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import HomeLayout from "./pages/HomeLayout";
+
 // import React from "react";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomeLayout />
+  },
+]);
 
 const App = () => {
-  return <div>App</div>;
+  return <RouterProvider router={router} />;
 };
 
 export default App;
